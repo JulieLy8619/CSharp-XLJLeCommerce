3dd1a984ab09fa34d1becabb9794cac640811213
juliely8619 <july8619@yahoo.com>
2019-02-22 01:22:55 -0800

    done with getters for now (3 failing but can't figureout why right now, think related to IDENTITY)

--- XLJLeCommerce-unittesting/UnitTest1.cs
+++ XLJLeCommerce-unittesting/UnitTest1.cs
@@ -43,22 +43,57 @@ namespace XLJLeCommerce_unittesting
 
         //product model ========================
         //getter name
+        [Fact]
+        public void TestGetProdName()
+        {
+            Product testProd1 = new Product();
+            testProd1.Name = "aName";
+            Assert.Equal("aName", testProd1.Name);
+        }
 
         //setter name
 
         //getter sku
+        [Fact]
+        public void TestGetProdSku()
+        {
+            Product testProd3 = new Product();
+            testProd3.Sku = "aSku";
+            Assert.Equal("aSku", testProd3.Sku);
+        }
 
         //setter sku
 
         //getter price
+        [Fact]
+        public void TestGetProdPrice()
+        {
+            Product testProd5 = new Product();
+            testProd5.Price = 20;
+            Assert.Equal(20, testProd5.Price);
+        }
 
         //setter price
 
         //getter description
+        [Fact]
+        public void TestGetProdDescription()
+        {
+            Product testProd7 = new Product();
+            testProd7.Description = "aDescription";
+            Assert.Equal("aDescription", testProd7.Description);
+        }
 
         //setter description
 
         //getter image url
+        [Fact]
+        public void TestGetProdImageURL()
+        {
+            Product testProd9 = new Product();
+            testProd9.ImageURL = "anImageURL";
+            Assert.Equal("anImageURL", testProd9.ImageURL);
+        }
 
         //setter image url
 
