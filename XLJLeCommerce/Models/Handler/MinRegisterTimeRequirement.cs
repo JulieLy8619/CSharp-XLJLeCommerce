60b742be92128ef29306c9839669590f655dac9f
xia LIU <xia.liu2@outlook.com>
2019-02-22 21:52:26 -0800

    added requirement for minregistertime

--- XLJLeCommerce/Models/Handler/MinRegisterTimeRequirement.cs
+++ XLJLeCommerce/Models/Handler/MinRegisterTimeRequirement.cs
@@ -1,11 +1,31 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Authorization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace XLJLeCommerce.Models.Handler
 {
-    public class MinRegisterTimeRequirement
+    public class MinRegisterTimeRequirement : AuthorizationHandler<MinRegisterTimeRequirement>,IAuthorizationRequirement
     {
+
+
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, MinRegisterTimeRequirement requirement)
+        {
+
+            DateTime dateOfRegister = Convert.ToDateTime(context.User.FindFirst(u => u.ValueType==ClaimValueTypes.DateTime).Value);
+
+            int min = DateTime.Today.Minute - dateOfRegister.Minute;
+            if (min >= 3)
+            {
+                context.Succeed(requirement);
+
+            }
+
+            return Task.CompletedTask;
+
+
+        }
     }
 }
