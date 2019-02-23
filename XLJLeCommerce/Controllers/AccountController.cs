9d6fef2e08eab27dd2e17712fe4a7493247989ed
juliely8619 <july8619@yahoo.com>
2019-02-23 01:36:58 -0800

    policy prod page works

--- XLJLeCommerce/Controllers/AccountController.cs
+++ XLJLeCommerce/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -23,6 +24,7 @@ namespace XLJLeCommerce.Controllers
 
 
         [HttpGet]
+        [AllowAnonymous]
         public IActionResult Register() => View();
 
         [HttpPost]
