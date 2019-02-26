bb2b8d9edecfa4451fbf20989136ab6620e585b0
juliely8619 <july8619@yahoo.com>
2019-02-26 11:18:34 -0800

    model created

---
+++ XLJLeCommerce/Models/Cart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XLJLeCommerce.Models
+{
+    public class Cart
+    {
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public int ProdID { get; set; }
+        public int ProdQty { get; set; }
+        public decimal TotalPrice { get; set; }
+    }
+}
