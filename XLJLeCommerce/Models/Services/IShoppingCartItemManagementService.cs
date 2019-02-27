9db5daecf76a35a35f0b2b780b69a75a81018a85
juliely8619 <july8619@yahoo.com>
2019-02-27 12:20:46 -0800

    added summary comments to shopping cart item

--- XLJLeCommerce/Models/Services/IShoppingCartItemManagementService.cs
+++ XLJLeCommerce/Models/Services/IShoppingCartItemManagementService.cs
@@ -9,49 +9,49 @@ namespace XLJLeCommerce.Models.Services
     public class IShoppingCartItemManagementService : IShoppingCartItem
     {
         /// <summary>
-        /// 
+        /// creates an item to add to the shopping cart
         /// </summary>
-        /// <param name="shoppingCartItem"></param>
-        /// <returns></returns>
+        /// <param name="shoppingCartItem">takes in a showpping cart item</param>
+        /// <returns>the completed service of adding it to the DB</returns>
         public Task Create(ShoppingCartItem shoppingCartItem)
         {
             throw new NotImplementedException();
         }
 
         /// <summary>
-        /// 
+        /// deletes a shopping cart item
         /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
+        /// <param name="id">which item to delete</param>
+        /// <returns>the task it was complete and delete from database</returns>
         public Task DeleteShoppingCartItem(int id)
         {
             throw new NotImplementedException();
         }
 
         /// <summary>
-        /// 
+        /// gets all shopping cart items
         /// </summary>
-        /// <returns></returns>
+        /// <returns>a list of the shopping cart items</returns>
         public Task<List<ShoppingCartItem>> GetAllShoppingCartItems()
         {
             throw new NotImplementedException();
         }
 
         /// <summary>
-        /// 
+        /// gets just one item shopping cart item
         /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
+        /// <param name="id">which item</param>
+        /// <returns>the item</returns>
         public Task<ShoppingCartItem> GetShoppingCartItem(int id)
         {
             throw new NotImplementedException();
         }
 
         /// <summary>
-        /// 
+        /// updates the shopping cart item
         /// </summary>
-        /// <param name="shoppingCartItem"></param>
-        /// <returns></returns>
+        /// <param name="shoppingCartItem">the shoping cart item</param>
+        /// <returns>the compelted task that it updated the database</returns>
         public Task UpdateShoppingCartItem(ShoppingCartItem shoppingCartItem)
         {
             throw new NotImplementedException();
