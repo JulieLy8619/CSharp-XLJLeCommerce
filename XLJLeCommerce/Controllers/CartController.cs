dad08e217e0b014f01b15fb58bd1465b9caa1c20
juliely8619 <july8619@yahoo.com>
2019-02-26 23:34:17 -0800

    added cart controller, interface and service

---
+++ XLJLeCommerce/Controllers/CartController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using XLJLeCommerce.Models.Interfaces;
+
+namespace XLJLeCommerce.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ICart _context;
+
+        public CartController(ICart context)
+        {
+            _context = context;
+        }
+
+    }
+}
