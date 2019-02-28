a623acef8238810df4e9f85b4c549410942c87f8
xia LIU <xia.liu2@outlook.com>
2019-02-27 17:41:26 -0800

    updated pickedproduc cs file under components folder under Models

--- XLJLeCommerce/Models/Components/PickedProduct.cs
+++ XLJLeCommerce/Models/Components/PickedProduct.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using XLJLeCommerce.Data;
 
 namespace XLJLeCommerce.Models.Components
 {
     public class PickedProduct
     {
-        
+        private CreaturesDbcontext _context;
+
+        public PickedProduct(CreaturesDbcontext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var products = _context.ShoppingCartTable.OrderByDescending(a => a.ID)
+                .ToList();
+            return View(products);
+        }
+
     }
 }
