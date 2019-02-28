20165d814033f6156c59e4c16894464aa62f21f6
xia LIU <xia.liu2@outlook.com>
2019-02-27 20:14:02 -0800

    updated account controller

--- XLJLeCommerce/Controllers/AccountController.cs
+++ XLJLeCommerce/Controllers/AccountController.cs
@@ -115,5 +115,103 @@ namespace XLJLeCommerce.Controllers
             return RedirectToAction("Index", "Home");
         }
 
+
+
+        [HttpPost]
+        public IActionResult ExternalLogin(string provider)
+        {
+            // Setting where we need to be redirected to once we have established our OAUTH
+            var redirectUrl = Url.Action(nameof(ExternalLoginCallback), "Account");
+            // the information for our OAUTH providers
+            var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
+            // Start the process
+            return Challenge(properties, provider);
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> ExternalLoginCallback(string error = null)
+        {
+            // If there is an error message, send them away
+            if (error != null)
+            {
+                // logging of the error coming in
+                TempData["Error"] = "Error with Provider";
+                return RedirectToAction("Login");
+            }
+
+            // see if our web app supports the login provider
+            var info = await _signInManager.GetExternalLoginInfoAsync();
+
+            // If the web app does not support the provider, make them login with a different technique.
+            if (info == null)
+            {
+                return RedirectToAction(nameof(Login));
+            }
+
+            // login with the external provider given the info from our sign in manager
+            var result = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false, bypassTwoFactor: true);
+
+            // redirect them home if the login was a success
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Product");
+            }
+
+  
+
+            //redirect to the external login page for the user to 
+            return View("ExternalLogin");
+
+        }
+
+
+        public async Task<IActionResult> ExternalLoginConfirmation(ExternalLoginViewModel elvm)
+        {
+            if (ModelState.IsValid)
+            {
+                var info = await _signInManager.GetExternalLoginInfoAsync();
+                if (info == null)
+                {
+                    TempData["Error"] = "Error loading information";
+                }
+
+                //Create the user.
+                //TODO: Could potentially force the user to add a password here....
+                //
+                var user = new ApplicationUser
+                {
+                    UserName = elvm.Email,
+                    Email = elvm.Email,
+                    FirstName = elvm.FirstName,
+                    LastName = elvm.LastName
+                };
+
+                var result = await _userManager.CreateAsync(user);
+
+                if (result.Succeeded)
+                {
+                    //TODO: Potentially add Claims here.....
+
+                    Claim fullNameClaim = new Claim("FullName", $"{user.FirstName} {user.LastName}");
+                    await _userManager.AddClaimAsync(user, fullNameClaim);
+
+                    result = await _userManager.AddLoginAsync(user, info);
+
+                    if (result.Succeeded)
+                    {
+                        // sign the user in with the information they gave us
+                        await _signInManager.SignInAsync(user, isPersistent: false);
+
+                        return RedirectToAction("Index", "Product");
+
+                    }
+                }
+
+
+            }
+            return View(elvm);
+        }
+
     }
 }
