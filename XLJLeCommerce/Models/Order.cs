920c4c938d5c7376b928c5aa68918254d7970121
juliely8619 <july8619@yahoo.com>
2019-03-02 19:18:13 -0800

    made classes for order and ordereditems

---
+++ XLJLeCommerce/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XLJLeCommerce.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+        public string UserID { get; set; }
+    }
+}
---
+++ XLJLeCommerce/Models/OrderedItems.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace XLJLeCommerce.Models
+{
+    public class OrderedItems
+    {
+        public int ID { get; set; }
+        public int OrderID { get; set; }
+        public int ShoppingCartItemID { get; set; }
+    }
+}
