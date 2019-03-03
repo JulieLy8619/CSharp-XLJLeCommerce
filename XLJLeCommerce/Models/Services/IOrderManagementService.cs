7c2da7ea47dbc9f0fcdcea62fd3161cde38d5c93
juliely8619 <july8619@yahoo.com>
2019-03-02 19:46:56 -0800

    did create order

--- XLJLeCommerce/Models/Services/IOrderManagementService.cs
+++ XLJLeCommerce/Models/Services/IOrderManagementService.cs
@@ -1,11 +1,34 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using XLJLeCommerce.Data;
+using XLJLeCommerce.Models.Interfaces;
 
 namespace XLJLeCommerce.Models.Services
 {
-    public class IOrderManagementService
+    public class IOrderManagementService : IOrder
     {
+        private CreaturesDbcontext _context { get; }
+
+        public IOrderManagementService(CreaturesDbcontext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// creates an order
+        /// </summary>
+        /// <param name="order">the order</param>
+        /// <returns>the task after it was added to the DB</returns>
+        public async Task CreateOrder(Order order)
+        {
+            if (await _context.OrderTable.FirstOrDefaultAsync(c => c.ID == order.ID) == null)
+            {
+                _context.OrderTable.Add(order);
+            }
+            await _context.SaveChangesAsync();
+        }
     }
 }
--- XLJLeCommerce/Models/Services/IOrderedItemsManagementService.cs
+++ XLJLeCommerce/Models/Services/IOrderedItemsManagementService.cs
@@ -2,10 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using XLJLeCommerce.Data;
+using XLJLeCommerce.Models.Interfaces;
 
 namespace XLJLeCommerce.Models.Services
 {
-    public class IOrderedItemsManagementService
+    public class IOrderedItemsManagementService : IOrderedItems
     {
+        private CreaturesDbcontext _context { get; }
+
+        public IOrderedItemsManagementService(CreaturesDbcontext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// creates an ordered item
+        /// </summary>
+        /// <param name="orderedItems">the ordered item</param>
+        /// <returns>the completed task after adding to the DB</returns>
+        public Task CreateOrderedItem(OrderedItems orderedItems)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// gets all the ordered items in an order
+        /// </summary>
+        /// <param name="id">id for which order</param>
+        /// <returns>ordered items in the order</returns>
+        public Task<IEnumerable<OrderedItems>> GetAllOrderedItems(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
