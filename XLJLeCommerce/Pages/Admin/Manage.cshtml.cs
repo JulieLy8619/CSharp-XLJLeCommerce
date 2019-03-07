589d3769ef428a154749c7ea094fb3ec7ca2f6e6
xia LIU <xia.liu2@outlook.com>
2019-03-06 22:43:25 -0800

    view all products for admin user and CRUD HAVE BEEN added for product

---
+++ XLJLeCommerce/Pages/Admin/Manage.cshtml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using XLJLeCommerce.Models;
+using XLJLeCommerce.Models.Interfaces;
+
+namespace XLJLeCommerce.Pages.Admin
+{
+    public class ManageModel : PageModel
+    {
+        private readonly Iproduct _product;
+        [FromRoute]
+        public int? ID { get; set; }
+
+        [BindProperty]
+        public Product Product { get; set; }
+
+        public ManageModel(Iproduct product, IConfiguration configuration)
+        {
+            _product= product;
+
+        }
+
+        public async Task OnGet()
+        {
+
+            Product = await _product.GetProduct(ID.GetValueOrDefault()) ?? new Product();
+
+        }
+
+
+
+        public async Task<IActionResult> OnPost()
+        {
+            
+            var pro = await _product.GetProduct(ID.GetValueOrDefault()) ?? new Product();
+
+            // set the data from the database to the new data from admin input
+            pro.Name = Product.Name;
+            pro.ImageURL = Product.ImageURL;
+            pro.Price = Product.Price;
+            pro.Sku = Product.Sku;
+
+            // Save the product in the database
+            await _product.UpdateProduct(pro);
+
+            return RedirectToPage("Admin", new { id = pro.ID });
+        }
+
+        public async Task<IActionResult> OnPostDelete()
+        {
+            await _product.DeleteProduct(ID.Value);
+            return RedirectToPage("Admin");
+        }
+
+    }
+}
---
+++ XLJLeCommerce/Pages/Admin/Productdetail.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using XLJLeCommerce.Models;
+using XLJLeCommerce.Models.Interfaces;
+
+namespace XLJLeCommerce.Pages.Admin
+{
+    public class ProductdetailModel : PageModel
+    {
+        private readonly Iproduct _product;
+        public ProductdetailModel(Iproduct product)
+        {
+            _product = product;
+        }
+
+        [FromRoute]
+        public int ID { get; set; }
+
+        public Product Product { get; set; }
+
+        public async Task OnGet()
+        {
+
+            Product = await _product.GetProduct(ID);
+        }
+    }
+}
