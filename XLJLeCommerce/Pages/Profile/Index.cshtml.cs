ebbfa28458463e7a660ee82d17975d8044e59b73
xia LIU <xia.liu2@outlook.com>
2019-03-08 10:35:41 -0800

    updated

--- XLJLeCommerce/Pages/Profile/Index.cshtml.cs
+++ XLJLeCommerce/Pages/Profile/Index.cshtml.cs
@@ -2,15 +2,34 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using XLJLeCommerce.Models;
 
 namespace XLJLeCommerce.Pages.Profile
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public IndexModel(
+        UserManager<ApplicationUser> userManager,
+        SignInManager<ApplicationUser> signInManager
+       )
         {
+            _userManager = userManager;
+            _signInManager = signInManager;
         }
+        [BindProperty]
+        public ApplicationUser AppUser { get; set; }
+        
+        public async Task OnGet()
+        {
+          AppUser= await _userManager.GetUserAsync(User);
+
+        }
+
     }
 }
