4387342b66f93d7bdcbadfcc97e6ed978cc27e79
xia LIU <xia.liu2@outlook.com>
2019-03-09 16:58:21 -0500

    fix the bug after update profile, log out, log back in, capture the new name show up in  the greeting part

--- XLJLeCommerce/Pages/Profile/Index.cshtml.cs
+++ XLJLeCommerce/Pages/Profile/Index.cshtml.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Authentication;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -41,14 +44,17 @@ namespace XLJLeCommerce.Pages.Profile
 
         public async Task<IActionResult> OnPost()
         {
+           
             var user = await _userManager.GetUserAsync(User);
-
+          var cc=User.Claims.FirstOrDefault(c => c.Type == "FullName");
+            await _signInManager.UserManager.RemoveClaimAsync(user, cc);
             user.FirstName = FirstName;
             user.LastName = LastName;
             user.Address = Address;
 
-
             await _userManager.UpdateAsync(user);
+            Claim fn= new Claim("FullName", $"{user.FirstName} {user.LastName}");
+           await _signInManager.UserManager.AddClaimAsync(user, fn);
             return RedirectToPage("/Profile/Index");
         }
 
